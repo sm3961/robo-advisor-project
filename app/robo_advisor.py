7eb4d43a8ac29282c0ca67b809ba35f26fae9cbe
sm3961 <65735941+sm3961@users.noreply.github.com>
2020-06-15 02:12:11 -0400

    Update robo_advisor.py

--- app/robo_advisor.py
+++ app/robo_advisor.py
@@ -10,14 +10,15 @@ import json
 request_url = "https://www.alphavantage.co/query?function=TIME_SERIES_DAILY_ADJUSTED&symbol=IBM&apikey=demo"
 
 response = requests.get(request_url)
-print(type(response))  #> <class 'requests.models.Response'>
-print(response.status_code)  #> 200
-print(response.text)   
 
 
 parsed_response = json.loads(response.text)
 
-breakpoint()
+last_refreshed = parsed_response["Meta Data"]["3. Last Refreshed"]
+
+latest_close = parsed_response["Time Series (Daily)"]["2020-06-12"]["4. close"]
+
+#breakpoint()
 
 
 
@@ -30,13 +31,13 @@ breakpoint()
 
 
 print("-------------------------")
-print("SELECTED SYMBOL: XYZ")
+print("SELECTED SYMBOL: IBM")
 print("-------------------------")
 print("REQUESTING STOCK MARKET DATA...")
 print("REQUEST AT: 2018-02-20 02:00pm")
 print("-------------------------")
-print("LATEST DAY: 2018-02-20")
-print("LATEST CLOSE: $100,000.00")
+print(f"LATEST DAY: {last_refreshed}")
+print(f"LATEST CLOSE: {latest_close}")
 print("RECENT HIGH: $101,000.00")
 print("RECENT LOW: $99,000.00")
 print("-------------------------")
